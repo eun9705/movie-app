2f0eae950d49a365f9cee0c1b39a7a82308bf8ea
eunkyoung <eun97052425@gmail.com>
2023-06-09 01:25:52 +0900

    modify : 영화 정보 받기 api 수정

--- src/Components/SearchForm.tsx
+++ src/Components/SearchForm.tsx
@@ -1,27 +1,37 @@
-import axios from "axios";
+import { useState } from "react";
 import { ImSearch } from 'react-icons/im';
+import { getMovieResApi } from "../api/serviceApi";
+import { MovieList } from "../types/interface";
+import SquareList from "./SquareList";
 
 const SearchForm = () => {
-    const api_key = process.env.REACT_APP_KMDB_KEY;
-    const submitHandler = async(e:React.ChangeEvent<HTMLFormElement>) => {
+    // const api_key = process.env.REACT_APP_KMDB_KEY;
+    const [listProps,setListProps] = useState<MovieList[] | undefined>([]);
+    
+    const submitHandler = async (e:React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
         let searchTypeVal = e.target.searchType.value;
         let searchContextVal = e.target.searchContext.value;
-        const res = await axios.get(`http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&${searchTypeVal}=${searchContextVal}&ServiceKey=${api_key}`);
-        console.log(res);
+        const result = await getMovieResApi(searchTypeVal,searchContextVal);
+        setListProps(result);
     }
-    return <form onSubmit={submitHandler} className="search-form">
-        <label htmlFor="search-type"></label>
-        <select name="searchType" id="search-type">
-            <option value="title">제목</option>
-            <option value="director">감독</option>
-        </select>
-        <label htmlFor="search-context"></label>
-        <input type="text" placeholder="검색어를 입력해주세요." name="searchContext" required id="search-context"/>
-        <button type="submit">
-            <ImSearch />
-        </button>    
-    </form>
+    return (
+        <div>
+            <form onSubmit={submitHandler} className="search-form">
+                <label htmlFor="search-type">검색유형선택</label>
+                <select name="searchType" id="search-type">
+                    <option value="title">제목</option>
+                    <option value="director">감독</option>
+                </select>
+                <label htmlFor="search-context">검색어입력</label>
+                <input type="text" placeholder="검색어를 입력해주세요." name="searchContext" required id="search-context"/>
+                <button type="submit">
+                    <ImSearch />
+                </button>
+            </form>
+            <SquareList listProps={listProps}/>
+        </div>
+    )
 }
 
 export default SearchForm;
