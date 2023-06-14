ca2756cd678b6e75a4f2f8e304e25448aec718ec
eunkyoung <eun97052425@gmail.com>
2023-06-14 22:58:35 +0900

    add : react-query 설치

--- src/Pages/MovieMain.tsx
+++ src/Pages/MovieMain.tsx
@@ -11,12 +11,14 @@ const MovieMain = () => {
     const [searchContext,setSearchContext] = useState<string>('');
     const [totalCount,setTotalCount] = useState<number | undefined>(0);
     const [listProps,setListProps] = useState<MovieList[] | undefined>([]);
+    const [viewList,setViewList] = useState<boolean>(false);
     const { keyword,context } = useParams() as { keyword:string;context: string};
     
     const navigation = useNavigate();
 
     const startApi = async (selectVal:string,inputVal:string) => {
         const result = await getMovieResApi(selectVal,inputVal);
+        setViewList(true);
         setTotalCount(result?.TotalCount);
         setListProps(result?.Result);
     }
@@ -27,21 +29,21 @@ const MovieMain = () => {
         startApi(searchType,searchContext);
     }
 
-    const checkState = async () => {
-        setSearchType(keyword);
-        setSearchContext(context);
-    }
+    // const checkState = async () => {
+    //     setSearchType(keyword);
+    //     setSearchContext(context);
+    // }
 
-    const searchHistory = async () => {
-        if(keyword && context) {
-            await checkState();
-            startApi(searchType,searchContext);
-        }  
-    }
+    // const searchHistory = async () => {
+    //     if(keyword && context) {
+    //         await checkState();
+    //         startApi(searchType,searchContext);
+    //     }  
+    // }
 
-    useEffect(()=>{
-        searchHistory();
-    },[keyword,context]);
+    // useEffect(()=>{
+    //     searchHistory();
+    // },[keyword,context]);
 
     
     return <>
@@ -59,27 +61,27 @@ const MovieMain = () => {
                     <SearchForm submitFn={submitHandler} searchType={searchType} setSearchType={setSearchType} searchContext={searchContext} setSearchContext={setSearchContext}/>
                 </div>
             </div>
-            {(keyword && context) &&
-                <div className='card-box container'>
-                    <strong className='result-text'>검색결과({totalCount})</strong>
-                    <hr />
-                    <div className="card-wrapper">
-                        {totalCount !==0 ? 
-                            listProps?.map((info,idx)=>{
-                                return <SquareList info={info} key={idx}/>
-                            }) :
-                            <div className='text-wrapper'>
-                                <strong><span className='highlight'>'{searchContext}'</span>에 대한 검색결과 없음</strong>
-                                <p>
-                                    단어의 철자가 정확한지 확인해 보세요.<br />
-                                    검색 옵션을 변경해서 다시 검색해 보세요.
-                                </p>
-                            </div>
-                        }
+                {viewList && 
+                    <div className='card-box container'>
+                        <strong className='result-text'>검색결과({totalCount})</strong>
+                        <hr />
+                        <div className="card-wrapper">
+                            {totalCount !==0 ? 
+                                listProps?.map((info,idx)=>{
+                                    return <SquareList info={info} key={idx}/>
+                                }) :
+                                <div className='text-wrapper'>
+                                    <strong><span className='highlight'>'{searchContext}'</span>에 대한 검색결과 없음</strong>
+                                    <p>
+                                        단어의 철자가 정확한지 확인해 보세요.<br />
+                                        검색 옵션을 변경해서 다시 검색해 보세요.
+                                    </p>
+                                </div>
+                            }
+                        </div>
+                        {(listProps && listProps?.length > 10) && <button className='more-btn'>View More +</button>}
                     </div>
-                    {(listProps && listProps?.length > 10) && <button className='more-btn'>View More +</button>}
-                </div>
-            }
+                }
         </div>
     </>
 }
