20572f0af4cbd2d155a81077dd42e9a6abb94a36
eunkyoung <eun97052425@gmail.com>
2023-07-09 20:58:25 +0900

    modify : 에러 페이지 및 api 요청 주소 수정

--- src/api/serviceApi.ts
+++ src/api/serviceApi.ts
@@ -1,24 +1,26 @@
 import axios from "axios";
 import { MovieData } from "../types/interface";
 
+const url = '/warn';
+
 export const getMovieResApi = async (searchType:string|undefined,searchContext:string|undefined) => {
     const api_key = process.env.REACT_APP_KMDB_KEY;
     try {
-        const { data } = await axios.get<MovieData>(`https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&${searchType}=${searchContext}&ServiceKey=${api_key}&listCount=150`);
+        const { data } = await axios.get<MovieData>(`http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&${searchType}=${searchContext}&ServiceKey=${api_key}&listCount=150`);
         return data.Data[0];
     }
     catch(error) {
-        window.location.href = 'https://eun9705.github.io/movie-app/warn';
+        window.location.pathname = url;
     }
 }
 
 export const getMovieDetail = async (id:string,seq:string) => {
     const api_key = process.env.REACT_APP_KMDB_KEY;
     try{
-        const { data } = await axios.get<MovieData>(`https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&movieId=${id}&movieSeq=${seq}&ServiceKey=${api_key}&listCount=1`);
+        const { data } = await axios.get<MovieData>(`http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&movieId=${id}&movieSeq=${seq}&ServiceKey=${api_key}&listCount=1`);
         return data.Data[0];
     }
     catch(error) {
-        window.location.href = 'https://eun9705.github.io/movie-app/warn';
+        window.location.pathname = url;
     }
 }
